08f82d9510a2d22cdbd02490bda0f6cb43fb1a04
kellyxc28 <kellyxiongc@gmail.com>
2024-05-15 15:06:46 -0400

    Completed Question 1: clickCounterHandler()

--- src/modify.js
+++ src/modify.js
@@ -1,9 +1,16 @@
 // These are your event handlers
 const clickCounterHandler = () => {
+  const clickButton = document.querySelector('#click-button');
+  clickButton.dataset.clicks = Number(clickButton.dataset.clicks) + 1;
+  if (clickButton.dataset.clicks < 2) {
+    clickButton.textContent = `I've been clicked ${clickButton.dataset.clicks} time.`;
+  } else {
+    clickButton.textContent = `I've been clicked ${clickButton.dataset.clicks} times!`;
+  }
+  console.log(clickButton.dataset.clicks);
 };
 
-const handleKeydown = () => {
-};
+const handleKeydown = () => {};
 
 // We've started this one for you
 const handleDelegation = (event) => {
@@ -11,16 +18,15 @@ const handleDelegation = (event) => {
   resultSpan.textContent = event.target.textContent;
 };
 
-const addNewRandomNumber = () => {
-};
-
+const addNewRandomNumber = () => {};
 
 // Select the elements and attach your event handlers inside main
 const main = () => {
   const delegationContainer = document.querySelector('#delegation');
   delegationContainer.addEventListener('click', handleDelegation);
 
-  
+  const incrementingButton = document.querySelector('#click-button');
+  incrementingButton.addEventListener('click', clickCounterHandler);
 };
 
 main();
